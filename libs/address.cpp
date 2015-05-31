2b24e5b7b9e0df13f19c12fec3437c7bbeea77bb
laboox <sina.labbaf@gmail.com>
2015-05-31 11:43:48 +0430

    libs phase 2

--- libs/address.cpp
+++ libs/address.cpp
@@ -6,9 +6,28 @@
 #include "address.h"
 
 string addrToString(address addr){
-    istringstream iss;
-    unsigned char ip[4];
+    ostringstream iss;
+    unsigned char ip[4] = {0};
+    for(int i=0;i<addr.size();i++){
+        ip[i/8] |= addr[i] << i%8;
+    }
+    iss<<(int)ip[0];
+    iss<<".";
+    iss<<(int)ip[1];
+    iss<<".";
+    iss<<(int)ip[2];
+    iss<<".";
+    iss<<(int)ip[3];
+    return iss.str();
 }
 
 address stringToAddr(string str){
+    address ret;
+    ret.reset();
+    unsigned char ip[4] = {0};
+    sscanf(str.c_str(), "%d.%d.%d.%d",ip, ip+1, ip+2, ip+3);
+    for(int i=0;i<ret.size();i++){
+        ret[i] = ip[i/8] & (1<<i%8);
+    }
+    return ret;
 }
