adfecd2c57b3b01a9886c27f0ac17a6a6e238968
aidamsv <moosavifar1993@yahoo.com>
2015-06-01 23:23:08 +0430

    Router without test

--- Router/Router.h
+++ Router/Router.h
@@ -4,24 +4,34 @@
 #include "Packet.h"
 #include "Sock.h"
 
-typedef pair<string, int> eth;
-typedef int fd;
+typedef pair<string, int> eth_fd;
+typedef pair<eth_fd, int> eth_fd_cost; 
 
-#define NAME first
+#define DEFAULT_COST 1
+#define ETH first.first
+#define FD first.second
 #define COST second
+#define DEST first
+#define INFINITY 1000
 
 class Router
 {
 public:
 	Router(int router_port);
+	void parse_cmd(string cmd);
 	void run();
 
 private:
 	int router_port;
 	int router_fd;
 
-	map<address, int> unicast_routing_table;
-	map<address, vector<address> > multicats_routing_table;
+	map<address, vector<address> > multicast_ip;
 	
-	vector<eth> ethernet_cards;
+	map<address, vector<eth_fd_cost> > routing_table;
+	map<string, vector<int> > fdsOfeth; 
+	
+	vector<string> ethernet_cards;
+	map<string, int> eth_cost;
+
+	map<string, int> connected_client;
 };
